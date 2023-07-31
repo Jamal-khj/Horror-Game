86f6cb0bd81a4d1439fd0f46a02b71eaf33f8923
Mehdi Ouldbeziou <m.beziou@student.saedubai.com>
2023-07-31 22:55:18 +0400

    Added Pause menu and the camera now follows the player

---
+++ Horror Game/Assets/Mehdi/pauseMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class pauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool isPaused;
+    public GameObject pausePanel;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+
+        }
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+        isPaused = true;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+
+        isPaused = false;
+    }
+
+
+}
